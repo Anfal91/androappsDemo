6168891e4b501418a21678bad26abe89dba9a79a
Anfal <anfalvaktapurya91@gmail.com>
2023-04-20 15:52:24 +0530

    first push

---
+++ js/common.js
@@ -0,0 +1,17 @@
+
+  $(document).ready(function(){
+    $('.fancybox').fancybox({
+      prevEffect: 'fade',
+      nextEffect: 'fade',
+      openEffect: 'fade',
+      openSpeed: 300,
+      closeSpeed: 300,
+      helpers: {
+	title: { type: 'inside'},
+	buttons: {}
+      }
+      });
+    
+  } );
+  
+  	
